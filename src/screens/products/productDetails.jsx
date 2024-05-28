1009c360bf295fa3cf23163086a28bb94771a2da
dineshs-toast <86337790+dineshs-toast@users.noreply.github.com>
2024-05-28 13:18:26 +0530

    Nav bar added to the products page

--- src/screens/products/productDetails.jsx
+++ src/screens/products/productDetails.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import TopNavBar from "./topNavigationMenu";
 
 export const ProductsList = () => {
     return <div>
+        <TopNavBar />
         <h1>Product Details</h1>
     </div>
 }
---
+++ src/screens/products/topNavigationMenu.jsx
@@ -0,0 +1,42 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary justify-content-between">
+      <Container fluid>
+        <Navbar.Brand href="#">
+            <img border="0" 
+                alt="Google Products" 
+                src="/ProductAppImages/gproducts.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0 justify-content-center flex-grow-1 pe-3"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Products</Nav.Link>
+            <Nav.Link href="#action3">Services</Nav.Link>
+            <Nav.Link href="#action4">About us</Nav.Link>
+            
+          </Nav>
+          <Form className="d-flex">
+            <Button variant="outline-success">Create Account</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
